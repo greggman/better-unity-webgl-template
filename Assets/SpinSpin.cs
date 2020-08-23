6004a3fe4d409da7bd4a6db52f542d7db8fb8ff8
Gregg Tavares <github@greggman.com>
2020-08-24 00:23:07 +0900

    Unity 2020

---
+++ Assets/SpinSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t.localEulerAngles = t.localEulerAngles + new Vector3(0, Time.deltaTime * 120.0f, 0);
+    }
+
+    Transform t;
+}
